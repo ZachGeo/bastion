bd1a315b47240cdbfe5891bccc180a961a19a67a
ZachGeo <zachageo95@gmail.com>
2024-02-28 21:34:08 +0200

    Set description for variables

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,49 +1,46 @@
 variable "instance_type" {
   default = "t2.micro"
-  description = ""
   type = string
 }
 
 variable "availability_zone" {
-  description = ""
   type = string
 }
 
 variable "vpc_security_group_ids" {
-  description = ""
-  type = list(string)
+  description = "Security group IDs of instance"
+  type        = list(string)
 }
 
 variable "subnet_id" {
-  description = ""
   type = string
 }
 
 variable "assign_eip" {
-  default = false
-  description = ""
-  type = bool
+  default     = false
+  description = "Boolean value to assign elastic IP or not on the ec2 instance"
+  type        = bool
 }
 
 variable "key_name" {
-  default = null
-  description = ""
-  type = string
+  default     = null
+  description = "SSH key name of ec2 instance"
+  type        = string
 }
 
 variable "public_key" {
-  default = ""
-  description = ""
-  type = string
+  default     = ""
+  description = "Filepath of public key"
+  type        = string
 }
 
 variable "instance_name" {
-  description = ""
-  type = string
+  description = "EC2 instance name"
+  type        = string
 }
 
 variable "assign_key_pair" {
-  default = false
-  description = ""
-  type = bool
+  default     = false
+  description = "Boolean value to create or not ssh key for ec2 instance"
+  type        = bool
 }
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,19 +1,17 @@
 variable "vpc_cidr_block" {
-  description = ""
   type = string
 }
 
 variable "public_subnet_cidr_block" {
-  description = ""
-  type = string
+  description = "CIDR of public subnet"
+  type        = string
 }
 
 variable "private_subnet_cidr_block" {
-  description = ""
-  type = string
+  description = "CIDR of private subnet"
+  type        = string
 }
 
 variable "availability_zone" {
-  description = ""
   type = string
 }
